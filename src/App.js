8cebda358a215f61ff40de0e628d24aabf99ca1f
Mazen <mazenadel19@gmail.com>
2021-07-09 17:35:34 +0200

    error handling

--- src/App.js
+++ src/App.js
@@ -6,24 +6,39 @@ import './App.css'
 function App() {
 	const [movies, setMovies] = useState([])
 	const [isLoading, setIsLoading] = useState(false)
+	const [error, setError] = useState(null)
 
 	async function fetchMoviesHandler() {
+		setError(null)
 		setIsLoading(true)
-		const response = await fetch('https://swapi.dev/api/films/')
-		const data = await response.json()
+		try {
+			const response = await fetch('https://swapi.dev/api/films/')
 
-		const transformedMovies = data.results.map(movieData => {
-			return {
-				id: movieData.episode_id,
-				title: movieData.title,
-				openingText: movieData.opening_crawl,
-				releaseDate: movieData.release_date,
+			if (!response.ok) {
+				throw new Error('something went wrong')
 			}
-		})
 
-		setMovies(transformedMovies)
+			const data = await response.json()
+			const transformedMovies = data.results.map(movieData => {
+				return {
+					id: movieData.episode_id,
+					title: movieData.title,
+					openingText: movieData.opening_crawl,
+					releaseDate: movieData.release_date,
+				}
+			})
+
+			setMovies(transformedMovies)
+		} catch (error) {
+			setError(error.message)
+		}
 		setIsLoading(false)
 	}
+
+	let content = <p>N0 M0V!3S F3TCHED Y3T</p>
+	if (isLoading) content = <p>L0AD!NG</p>
+	if (error) content = <p>{error}</p>
+	if (movies.length > 0) content = <MoviesList movies={movies} />
 
 	return (
 		<React.Fragment>
@@ -31,23 +46,7 @@ function App() {
 				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
 			</section>
 
-			{!isLoading && movies.length === 0 && (
-				<section>
-					<p>N0 M0V!3S F3TCHED Y3T</p>
-				</section>
-			)}
-
-			{isLoading && (
-				<section>
-					<p>L0AD!NG</p>
-				</section>
-			)}
-
-			{!isLoading && movies.length > 0 && (
-				<section>
-					<MoviesList movies={movies} />
-				</section>
-			)}
+			<section>{content}</section>
 		</React.Fragment>
 	)
 }
